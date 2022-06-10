72e2a0bfc31c8f2e5d457589881f5236b59db5eb
Samuel Safahi <60489622+samisbakedham@users.noreply.github.com>
2022-06-09 23:26:05 -0700

    concert

--- test/shared/externalFixtures.ts
+++ test/shared/externalFixtures.ts
@@ -2,7 +2,7 @@ import {
   abi as FACTORY_ABI,
   bytecode as FACTORY_BYTECODE,
 } from '@cndllabs/v3-core/artifacts/contracts/UniswapV3Factory.sol/UniswapV3Factory.json'
-import { abi as FACTORY_V2_ABI, bytecode as FACTORY_V2_BYTECODE } from '@uniswap/v2-core/build/UniswapV2Factory.json'
+import { abi as FACTORY_V2_ABI, bytecode as FACTORY_V2_BYTECODE } from '@cndllabs/v2-core/build/UniswapV2Factory.json'
 import { Fixture } from 'ethereum-waffle'
 import { ethers, waffle } from 'hardhat'
 import { IUniswapV3Factory, IWETH9, MockTimeSwapRouter } from '../../typechain'
